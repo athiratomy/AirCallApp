fb861f13eb10c5481a40404ee4f479088e74acdf
athiratomy <87834646+athiratomy@users.noreply.github.com>
2022-09-10 00:55:16 -0400

    Update ActivityDetail.js

--- code/AirCall/src/components/ActivityDetail.js
+++ code/AirCall/src/components/ActivityDetail.js
@@ -2,9 +2,14 @@ import React, { useState } from "react";
 
 import classes from "./ActivityDetail.module.css";
 
-const ActivityDetail = (props) => {
-  const [item, setItem] = useState([]);
 
+const ActivityDetail = (props) => {
+ 
+  const [item, setItem] = useState([]);  
+  const [archiveCheck, setArchiveCheck] = useState('');
+ var requestOptions;
+ 
+  
   function fetchItemHandler(event) {
     console.log(event.target.value);
     var apiLink =
@@ -15,37 +20,55 @@ const ActivityDetail = (props) => {
         return response.json();
       })
       .then((data) => {
-        setItem(data);
-        console.log(item);
+        setItem(data);        
       });
   }
 
   const archiveHandler = (event) => {
     var apiLink =
-      "https://aircall-job.herokuapp.com/activities/" + event.target.value;
-    console.log(apiLink);
+      "https://aircall-job.herokuapp.com/activities/" + event.target.value;   
+      fetch(apiLink)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setArchiveCheck(data.is_archived);
+        console.log(archiveCheck) ;      
+      });
 
-    const requestOptions = {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ is_archived: true }),
-    };
+    if(archiveCheck === true) {
+       requestOptions = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ is_archived: false }),
+      };     
+     
+    }
+    else{
+      requestOptions = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ is_archived: true }),
+      };   
+       
+    }    
 
     fetch(apiLink, requestOptions)
       .then((response) => {
         return response.json();
       })
       .then((data) => {        
-        alert("Call history archived successfully");
+        props.onDataChange(data);
       });
+      
   };
 
   return (
-    <li className={classes.call}>
-      <h2>{props.title} call</h2>
+    <li className={classes.call}>      
       <h3>From: {props.fromCall}</h3>
       <h3>To: {props.toCall}</h3>
       <h3>{props.duration} minutes</h3>
+      <h4>{props.title} call</h4>
       <button
         className={classes.detail}
         value={props.id}
@@ -53,15 +76,17 @@ const ActivityDetail = (props) => {
       >
         Details
       </button>
+
       <button
-        className={classes.detail}
+        className={classes.detail}        
         value={props.id}
         onClick={archiveHandler}
       >
-        Archive
-      </button>
+       Archive/Unarchive
+      </button>               
       <h3>{item.via}</h3>
       <h3>{item.created_at}</h3>
+      
     </li>
   );
 };
