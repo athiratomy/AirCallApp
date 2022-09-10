6dffef9e3c20ac90d73cac10dcfde96ee8c6af6c
athiratomy <87834646+athiratomy@users.noreply.github.com>
2022-09-10 00:54:37 -0400

    Update ActivityFeed.js

--- code/AirCall/src/components/ActivityFeed.js
+++ code/AirCall/src/components/ActivityFeed.js
@@ -4,15 +4,23 @@ import ActivityDetail from "./ActivityDetail";
 import classes from "./ActivityFeed.module.css";
 
 const ActivityFeed = (props) => {
+
+  const dataChangeHandler = (changedData) => {    
+    const newCallData = {...changedData};
+    props.onSaveData(newCallData);   
+  }
+
   return (
     <ul className={classes["calls-list"]}>
-      {props.callHistory.map((data) => (
+      {props.callHistory.map((data) => (        
         <ActivityDetail
           id={data.id}
           title={data.title}
           duration={data.duration}
           fromCall={data.fromCall}
           toCall={data.toCall}
+          isArchived={data.is_archived}
+          onDataChange={dataChangeHandler}
         />
       ))}
     </ul>
